fc658a6454f2cacbdb775239565398c45ad4e645
jsyum <jennifersyum@gmail.com>
2019-02-08 21:58:37 -0500

    random number variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
 //few global variables
 var losses = 0;
 var wins = 0;
-var computerChoice;
-// Math.floor(Math.random()*(102)+19)
+//value assigned to computer choice
+var computerChoice = Math.floor(Math.random() * 102 + 19);
+console.log(computerChoice);
 
-//values loaded into crystals 1-4
+//values assigned to crystals 1-4
 var crys1 = Math.floor(Math.random() * 12) + 1;
+console.log(crys1);
 var crys2 = Math.floor(Math.random() * 12) + 1;
+console.log(crys2);
 var crys3 = Math.floor(Math.random() * 12) + 1;
+console.log(crys3);
 var crys4 = Math.floor(Math.random() * 12) + 1;
+console.log(crys4);
 
 // $("crystal_1").each(function() {
 //   var number = Math.floor(Math.random() * 12) + 1;
